2ef4fcb7393a056e57408aef9ed7250768fd8cbd
andrewlam <andrewlam@tamu.edu>
2017-03-21 21:51:07 -0500

    Delete Main.java

--- src/Main.java
+++
@@ -1,8 +0,0 @@
-public class Main{
-	public static void main(String... args){
-		Board myBoard = new Board(6,4); 
-		Kalah myKalah = new Kalah(myBoard);
-		myKalah.displayIntro();
-		myKalah.play();
-	}
-}
