59bf12d9487757253d1eadc6b9f3e954a27e83ef
Nathan <nateleake@tamu.edu>
2017-03-27 21:47:21 -0500

    removed unecessary imports

--- new src/Main.java
+++ new src/Main.java
@@ -1,22 +1,3 @@
-import java.awt.BorderLayout;
-import java.awt.Color;
-import java.awt.Dimension;
-import java.awt.GridLayout;
-import java.awt.Image;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
-
-import javax.swing.BorderFactory;
-import javax.swing.BoxLayout;
-import javax.swing.ImageIcon;
-import javax.swing.JButton;
-import javax.swing.JFrame;
-import javax.swing.JLabel;
-import javax.swing.JPanel;
-import javax.swing.SwingConstants;
-import javax.swing.WindowConstants;
-
-@SuppressWarnings("unused")
 class Main{
 	public static void main(String... args){
 		Utils.createMenuWindow();
