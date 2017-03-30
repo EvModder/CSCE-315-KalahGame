333873eaa312205bfcc2b7a30402bbb159f6156b
GitHub <myoutsidebuisnessemail@gmail.com>
2017-03-29 20:07:44 -0500

    separated out Settings

--- src/Main/KalahSquare.java
+++
@@ -1,8 +0,0 @@
-package Main;
-
-
-interface KalahSquare{
-	abstract void addSeeds(int i);
-	abstract void setSeeds(int i);
-	abstract int getSeeds();
-}
--- src/Main/Main.java
+++
@@ -1,8 +0,0 @@
-package Main;
-
-class Main{
-	public static void main(String... args){
-		Utils.openMenuWindow();
-		
-	}
-}
