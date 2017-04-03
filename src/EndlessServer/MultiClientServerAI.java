bdb10a2a688fbcafe48fbc7b03be292cddbaac8f
Nathan <nateleake@tamu.edu>
2017-04-02 19:41:34 -0500

    new servers

--- src/EndlessServer/MultiClientServerAI.java
+++ src/EndlessServer/MultiClientServerAI.java
@@ -1,35 +1,41 @@
 package EndlessServer;
 import java.util.Random;
-
+import GUI.MultiClientServerWindow;
+import GUI.NotGUIManager;
+import Main.KalahGame;
 import Main.Settings;
 import ServerUtils.Connection;
 import ServerUtils.MultiServer;
 import ServerUtils.MultiServer.ConnectionReceiver;
 
 class MultiClientServerAI implements ConnectionReceiver{
-	/*
-	 * A multi-client version of EndlessServerAI. This version waits for client
+	/* A multi-client version of EndlessServerAI. This version waits for client
 	 * connections and starts a new game with each new connection it receives.
 	 */
 	Random rand = new Random();
 	
-	public static void main(String... args){
-		Settings.changeSetting("play-as-AI", "true");
-		
-		new MultiServer(new MultiClientServerAI());
+	public static void main(String... args){new MultiClientServerAI();}
+	
+	MultiClientServerAI(){
+		new MultiClientServerWindow();
+		new MultiServer(this);
 	}
 
 	@Override
 	public void gotConnection(Connection connection){
 		new Thread(){@Override public void run(){
-			Settings.changeSetting("holes-per-side", String.valueOf(rand.nextInt(9)+2));
-			Settings.changeSetting("seeds-per-hole", String.valueOf(rand.nextInt(10)+1));
-			Settings.changeSetting("time-limit", String.valueOf(rand.nextInt(49001)+1000));
-			Settings.changeSetting("starting-player", rand.nextBoolean() ? "S" : "F");
-			
-			KalahNoGUI game = new KalahNoGUI(true, connection);
-			while(!game.gameOver) Thread.yield();
-			
+			//Randomize settings
+			Settings settings = new Settings();
+			settings.set("play-as-AI", true);
+			settings.set("is-server", true);
+			settings.set("holes-per-side", rand.nextInt(8)+3);
+			settings.set("seeds-per-hole", rand.nextInt(10)+1);
+			settings.set("time-limit", rand.nextInt(49001)+1000);
+			settings.set("starting-player", rand.nextBoolean() ? "S" : "F");
+			settings.set("game-type", rand.nextBoolean() ? "S" : "R");
+
+			//Start a new game
+			new KalahGame(new NotGUIManager(), settings, connection);
 		}}.start();
 	}
 }
