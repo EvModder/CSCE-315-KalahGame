84b5cb698885102d2c45d3e48cab233e3669aaf0
Nathan <nateleake@tamu.edu>
2017-04-02 19:41:59 -0500

    Sorted GUI into new package

---
+++ src/NotGUIManager.java
@@ -0,0 +1,14 @@
+package GUI;
+
+
+public class NotGUIManager extends GUIManager{
+	@Override public void openMenuWindow(){}
+	@Override public void closeMenuWindow(){}
+	@Override public void openWaitingWindow(){}
+	@Override public void closeWaitingWindow(){}
+	@Override public void connectionErrorWindow(){}
+	@Override public void openBoardWindow(int[] board){}
+	@Override public void closeBoardWindow(){}
+	@Override public void openGameOverWindow(GameResult ending){}
+	@Override public void openGameErrorWindow(String message){}
+}
