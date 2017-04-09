6a8fbc088d1cc1815150bf98437584c8c92cc3ec
GitHub <myoutsidebuisnessemail@gmail.com>
2017-04-09 17:17:28 -0500

    removed things

--- src/old/Main.java
+++
@@ -1,9 +0,0 @@
-package old;
-public class Main{
-	public static void main(String... args){
-		Board myBoard = new Board(6,4); 
-		Kalah myKalah = new Kalah(myBoard);
-		myKalah.displayIntro();
-		myKalah.play();
-	}
-}
