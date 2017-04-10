d71997d86bde5758e9c7cc483ceb8221024825a0
GitHub <myoutsidebuisnessemail@gmail.com>
2017-04-09 22:21:11 -0500

    mass-delete

--- src/AI/MagicGameFinder.java
+++
@@ -1,7 +0,0 @@
-package AI;
-
-public class MagicGameFinder {
-	public static void main(String... args){
-//		Board board = new Board(6, 4);
-	}
-}
--- src/Main/KalahSquare.java
+++
@@ -1,7 +0,0 @@
-package Main;
-
-public interface KalahSquare{
-	abstract void addSeeds(int i);
-	abstract void setSeeds(int i);
-	abstract int getSeeds();
-}
